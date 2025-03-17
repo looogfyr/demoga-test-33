b09cc4af5f8a7f2108dea3a8b1df385c0330c3b4
Alex Dubinin <alexdubinin88@gmail.com>
2025-03-17 18:45:30 +0900

    added DemoTestQA

---
+++ src/test/java/DemoTestQA.java
@@ -0,0 +1,14 @@
+
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class DemoTestQA {
+@Test
+    void fiilFormTest() {
+    open();
+}
+}
+
+
