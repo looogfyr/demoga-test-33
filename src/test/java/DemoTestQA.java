ec647ae7881bc292e9ca61bd0a20e8a5cf8a4767
Alex Dubinin <alexdubinin88@gmail.com>
2025-03-17 23:05:40 +0900

    added DemoTestQA-TextBox

--- src/test/java/DemoTestQA.java
+++ src/test/java/DemoTestQA.java
@@ -1,14 +1,36 @@
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class DemoTestQA {
-@Test
+   @BeforeAll
+   static void beforeall(){
+       Configuration.browserSize = "1600x900";
+       Configuration.baseUrl = "https://demoqa.com";
+       Configuration.pageLoadStrategy = "eager";
+   }
+
+    @Test
     void fiilFormTest() {
-    open();
-}
+
+        open("/text-box");
+        $("#userName").setValue("Alexey");
+        $("#userEmail").setValue("AlexGG@BBC.com");
+        $("#currentAddress").setValue("Great Beketov Street");
+        $("#permanentAddress").setValue("Big Gtreat Beketov Street");
+        $("#submit").click();
+        sleep(3000);
+
+        $("#output #name").shouldHave(text("Alexey"));
+        $("#output #email").shouldHave(text("AlexGG@BBC.com"));
+        $("#output #currentAddress").shouldHave(text("Great Beketov Street"));
+        $("#output #permanentAddress").shouldHave(text("Big Gtreat Beketov Street"));
+
+    }
 }
 
 
